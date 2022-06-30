d55f99baf2b27ea34723f0709703f2b3ed740b43
Adam Mazurkiewicz-Madej <adam.mazurkiewicz@ventum.com>
2022-06-30 16:38:17 +0200

    rainbow

--- src/sketch.js
+++ src/sketch.js
@@ -40,12 +40,29 @@ const iit = (point) => {
   return isInTriangle(A, B, C, LENGTH, point);
 }
 
+const Point = (x, y) => ({x, y});
+
+const middle = (p1, p2) => ({
+  x: (p1.x + p2.x) / 2,
+  y: (p1.y + p2.y) / 2,
+})
+
+//    █████████  ████
+//   ███░░░░░███░░███
+//  ███     ░░░  ░███   ██████    █████   █████
+// ░███          ░███  ░░░░░███  ███░░   ███░░
+// ░███          ░███   ███████ ░░█████ ░░█████
+// ░░███     ███ ░███  ███░░███  ░░░░███ ░░░░███
+//  ░░█████████  █████░░████████ ██████  ██████
+
+
 class Fader {
   min;
   max;
   step;
   current;
   currentStep;
+  currentStepInc;
 
   exponential;
 
@@ -63,20 +80,15 @@ class Fader {
     };
   }
 
-  val = (debug = false) => {
+  val = () => {
     let result = this.current;
 
-    debug && console.log("if", this.currentStep)
     if (this.currentStep) {
 
       if (this.exponential)
         this.currentStep += this.currentStep
 
-      debug && console.log(this.currentStep)
       this.current += this.currentStep;
-      debug && console.log("current", this.current)
-      debug && console.log("max", this.max)
-      debug && console.log("min", this.min)
 
       if (this.current > this.max) {
         this.current     = this.max;
@@ -102,18 +114,20 @@ class Fader {
   };
 }
 
-const Point = (x, y) => ({x, y});
-
-
+//  █████   █████
+// ░░███   ░░███
+//  ░███    ░███   ██████   ████████   █████
+//  ░███    ░███  ░░░░░███ ░░███░░███ ███░░
+//  ░░███   ███    ███████  ░███ ░░░ ░░█████
+//   ░░░█████░    ███░░███  ░███      ░░░░███
+//     ░░███     ░░████████ █████     ██████
 
 const middleW = window.innerWidth / 2
 const middleH = window.innerHeight / 2
-console.log(innerWidth)
-console.log(innerHeight)
 
 
 // const LENGTH      = TAN_60 * innerHeight; // px
-const LENGTH      = innerWidth; // px
+const LENGTH      = window.innerWidth; // px
 const HALF_LENGTH = (LENGTH / 2); // px
 
 
@@ -125,22 +139,103 @@ const POINTS      = [];
 const EDGE_POINTS = [A, B, C];
 
 
+let firstClick      = true;
+
+// let firstPoint      = undefined;
+// let firstPointValid = false;
+// let lastPoint = undefined;
+
+let firstPoint      = middle(A, B);
+let firstPointValid = true;
+let lastPoint = firstPoint
+
+
+let adding          = true;
+
+
+let counter = 0;
+let colorCounter = 0;
+
+let shapeCanvas;
+
+
+const MAXIMAL_MAX_TO_ADD_LUL = 150;
+let maxToAdd                 = 1;
+let maxToAddStep             = 0.01;
+
+let hideContour = false;
+
+//  ██████████              █████                              ██████
+// ░░███░░░░░█             ░░███                              ███░░███
+//  ░███  █ ░  █████ █████ ███████   ████████   ██████       ░███ ░░░  █████ ████ ████████    ██████   █████
+//  ░██████   ░░███ ░░███ ░░░███░   ░░███░░███ ░░░░░███     ███████   ░░███ ░███ ░░███░░███  ███░░███ ███░░
+//  ░███░░█    ░░░█████░    ░███     ░███ ░░░   ███████    ░░░███░     ░███ ░███  ░███ ░███ ░███ ░░░ ░░█████
+//  ░███ ░   █  ███░░░███   ░███ ███ ░███      ███░░███      ░███      ░███ ░███  ░███ ░███ ░███  ███ ░░░░███
+//  ██████████ █████ █████  ░░█████  █████    ░░████████     █████     ░░████████ ████ █████░░██████  ██████
+
 const getRandomInt       = (max, min = 0) => Math.floor(Math.random() * (max - min)) + min;
 const getRandomEdgePoint = () => EDGE_POINTS[getRandomInt(3)];
 const getRandomPoint     = () => Point(getRandomInt(B.x, A.x), getRandomInt(B.y, C.y));
 
-const F         = new Fader(0, 0, 200, 2, true);
+const F         = new Fader(0, 0, 200, 2, 1 );
 const dotFader  = new Fader(0, 0, 200, 2).fadeIn();
 const sizeFader = new Fader(200, 5, 400, 4, 0).fadeOut();
 
-let firstClick      = true;
-let firstPoint      = undefined;
-let firstPointValid = false;
-let adding          = true;
 
-let lastPoint = undefined;
 
-const addNextPoint = () => {
+const PointMode = {
+  PIXEL: "pixel",
+  DOT  : "dot"
+}
+
+
+const setPointsOnCanvas = (points, mode = PointMode.DOT) => {
+
+  push();
+  colorMode(HSB, 360);
+
+  let pink = color(colorCounter++ % 360, 360, 360, 180);
+
+  pop();
+
+  switch (mode) {
+    case PointMode.PIXEL: {
+      shapeCanvas.loadPixels();
+
+      points.forEach((p) => {
+        let index = 4 * (Math.round(p.y) * width + Math.round(p.x));
+
+        shapeCanvas.pixels[index]     = red(pink);
+        shapeCanvas.pixels[index + 1] = green(pink);
+        shapeCanvas.pixels[index + 2] = blue(pink);
+        shapeCanvas.pixels[index + 3] = alpha(pink);
+
+
+        shapeCanvas.updatePixels();
+      });
+    }
+      break;
+
+    case PointMode.DOT: {
+      const c = shapeCanvas;
+
+      c.colorMode(HSB, 360);
+      c.fill(pink);
+
+      // c.fill(pink);
+      c.noStroke();
+
+      points.forEach((p) => {
+        c.circle(p.x, p.y, 5)
+      });
+    }
+      break;
+  }
+}
+
+const addPoints = (n = 1) => {
+  let newPoints = [];
+
   let next;
 
   // do {
@@ -149,35 +244,70 @@ const addNextPoint = () => {
   // } while (!iit(next));
   // console.log(next);
 
-  let rand = getRandomEdgePoint();
+  for (let i = 0; i < n; i++) {
 
-  next = {
-    x: Math.abs(rand.x + lastPoint.x) / 2,
-    y: Math.abs(rand.y + lastPoint.y) / 2,
+    let rand = getRandomEdgePoint();
+
+    next = {
+      x: Math.abs(rand.x + lastPoint.x) / 2,
+      y: Math.abs(rand.y + lastPoint.y) / 2,
+    }
+
+    // shapeCanvas.loadPixels();
+    if (lastPoint !== firstPoint) {
+      // shapeCanvas.set(next.x, next.y, color(255));
+      newPoints.push(next);
+      // POINTS.push(lastPoint);
+    }
+    // shapeCanvas.updatePixels();
+
+    lastPoint = next;
   }
 
-  if (lastPoint != firstPoint)
-    POINTS.push(lastPoint);
-
-  lastPoint = next;
+  setPointsOnCanvas(newPoints);
 }
-
+//  ██████   ██████            ███
+// ░░██████ ██████            ░░░
+//  ░███░█████░███   ██████   ████  ████████
+//  ░███░░███ ░███  ░░░░░███ ░░███ ░░███░░███
+//  ░███ ░░░  ░███   ███████  ░███  ░███ ░███
+//  ░███      ░███  ███░░███  ░███  ░███ ░███
+//  █████     █████░░████████ █████ ████ █████
 
 function setup() {
 
   createCanvas(window.innerWidth, window.innerHeight);
 
+  shapeCanvas = createGraphics(width, height);
 
+  frameRate(60)
+
+
+  setTimeout(() => hideContour = true, 1000);
+  F.fadeIn();
 }
 
-const MAXIMAL_MAX_TO_ADD_LUL = 50;
-let maxToAdd                 = 1;
-let maxToAddStep             = 0.01;
-console.log(A, B, C)
 
 function draw() {
-  // background(32, 72, 133);
-  background(0);
+  if (firstPoint) {
+    // fill(firstPointValid ? 0 : 255, firstPointValid ? 255 : 0, 0, dotFader.val());
+    // fill(255, 0, 0, dotFader.val());
+    // circle(firstPoint.x, firstPoint.y, firstPointValid ? sizeFader.val() : 30);
+
+    if (adding && firstPointValid) {
+      setTimeout(() => {
+        addPoints(Math.floor(maxToAdd));
+
+        if (maxToAdd < MAXIMAL_MAX_TO_ADD_LUL)
+          maxToAddStep += 0.001;
+        maxToAdd += maxToAddStep;
+      }, 0);
+    }
+  }
+
+
+  background(32, 72, 133);
+  // background(0);
   // background(255);
 
   noStroke();
@@ -186,7 +316,7 @@ function draw() {
 
   if (iit(Point(mouseX, mouseY))) {
     F.fadeIn();
-  } else {
+  } else if (hideContour) {
     F.fadeOut();
   }
 
@@ -200,48 +330,10 @@ function draw() {
 
   // push();
   let pointLength = POINTS.length;
-  // colorMode(HSB, pointLength);
 
-  let pink = color(255);
-  loadPixels();
-
-  let d = pixelDensity();
-
-  POINTS.forEach((p, i) => {
-    // fill(i, pointLength, pointLength, pointLength * 0.4);
-    // fill(i, 0, 0, pointLength);
-    // circle(p.x, p.y, 2);
-    let index;// = 4 * ((p.y * d + j) * width * d + (p.x * d + i));
-
-    for (let i = 0; i < d; i++) {
-      for (let j = 0; j < d; j++) {
-        index = 4 * ((Math.round(p.y) * d + j) * width * d + (Math.round(p.x) * d + i));
-
-        pixels[index]     = red(pink);
-        pixels[index + 1] = green(pink);
-        pixels[index + 2] = blue(pink);
-        pixels[index + 3] = alpha(pink);
-      }
-    }
-  });
-  updatePixels();
+  image(shapeCanvas, 0, 0);
 
 
-  if (firstPoint) {
-    // fill(firstPointValid ? 0 : 255, firstPointValid ? 255 : 0, 0, dotFader.val());
-    // fill(255, 0, 0, dotFader.val());
-    // circle(firstPoint.x, firstPoint.y, firstPointValid ? sizeFader.val() : 30);
-
-    if (adding && firstPointValid) {
-      for (let i = 0; i < Math.floor(maxToAdd); i++) {
-        addNextPoint();
-      }
-
-      if (maxToAdd < MAXIMAL_MAX_TO_ADD_LUL)
-        maxToAddStep += 0.001;
-      maxToAdd += maxToAddStep;
-    }
-  }
 
 }
 
@@ -263,7 +355,6 @@ function mouseClicked() {
     adding = !adding;
   }
 }
-
 
 
 
